dccdd51bee19615dc69cd4c7daa966da9faaae40
AlexMartinDev01 <alexmartindev01@gmail.com>
2026-09-23 19:51:39 +0800

    Fix V1.4.0 Intent import

--- ShiguangBox/app/src/main/java/com/shiguangbox/app/MainActivity.kt
+++ ShiguangBox/app/src/main/java/com/shiguangbox/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shiguangbox.app
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Build

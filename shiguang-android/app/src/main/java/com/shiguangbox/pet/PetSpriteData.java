35a867a631d4e8f88c5c298e14036f32585bd7f9
AlexMartinDev01 <alexmartindev01@gmail.com>
2026-09-23 17:29:45 +0800

    Use compact embedded sprite sheet

--- shiguang-android/app/src/main/java/com/shiguangbox/pet/PetSpriteData.java
+++ shiguang-android/app/src/main/java/com/shiguangbox/pet/PetSpriteData.java
@@ -2,13 +2,11 @@ package com.shiguangbox.pet;
 
 final class PetSpriteData {
     static String data() {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(26000);
         sb.append(PetSpriteChunk0.data());
         sb.append(PetSpriteChunk1.data());
         sb.append(PetSpriteChunk2.data());
         sb.append(PetSpriteChunk3.data());
-        sb.append(PetSpriteChunk4.data());
-        sb.append(PetSpriteChunk5.data());
         return sb.toString();
     }
 }
